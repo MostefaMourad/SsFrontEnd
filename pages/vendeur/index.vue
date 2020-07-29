b0fb9ada4ba50655d6c68a6c25722fcb6d50427c
MostefaMourad <hm_mostefa@esi.dz>
2020-07-29 14:29:44 +0100

    Vendeur UI

--- pages/vendeur/index.vue
+++ pages/vendeur/index.vue
@@ -1,13 +1,189 @@
 <template>
     <v-container>
         <v-row>
+            <v-col align="center">
+                <h1>
+                    VENDEZ SUR SOS SHOP ET GAGNER DE L'ARGENT
+                </h1>
+            </v-col>
+        </v-row>
+        <v-row>
+        <v-card
+        width="100%" 
+        >
+        <v-tabs
+            background-color="white"
+            color="primary"
+            centered
+            >
+            <v-tab>Vendez sur Sos Shop </v-tab>
+            <v-tab>Aide aux vendeurs </v-tab>
+
+            <v-tab-item
+            >
+            <v-col align="center">
+                <h1 class="primary--text">
+                    Créez votre boutique en ligne et vendez partout en algerie
+                </h1>
+            </v-col>
+            <v-container>
+                <v-col align="center">
+                    <h2 class="primary-text"> Comment faire ? </h2>
+                    &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+                    <v-row align="center">
+                        <v-col md="4" align="right">
+                            <v-avatar size="100">
+                            <img
+                                src="https://cdn.vuetifyjs.com/images/john.jpg"
+                                alt="John"
+                            >
+                            </v-avatar>
+                        </v-col>
+                        <v-col md="8" align="left">
+                            <ul>
+                                <h2 class="primary--text"> Etap1 : Inscrivez-vous en 5 minutes </h2>
+                                <li>
+                                    Remplissez le formulaire d’inscription.
+                                </li>
+                                <li>
+                                    Lisez et approuvez notre e-contrat.
+                                </li>
+                            </ul>
+                        </v-col>
+                    </v-row>
+                    &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+                    <v-row>
+                        <v-col md="4" align="right">
+                            <v-avatar size="100">
+                            <img
+                                src="https://cdn.vuetifyjs.com/images/john.jpg"
+                                alt="John"
+                            >
+                            </v-avatar>
+                        </v-col>
+                        <v-col md="8" align="left">
+                            <ul>
+                                <h2 class="primary--text"> Etape 2: Devenez un expert du e-commerce </h2>
+                                <li>
+                                    Visualisez nos formations pour les nouveaux vendeurs et apprenez à vendre en ligne.
+                                </li>
+                                <li>
+                                    Activez votre compte Vendeur par un appel avec notre service et gérez votre boutique en ligne.
+                                </li>
+                            </ul>
+                        </v-col>
+                    </v-row>
+                    &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+                    <v-row>
+                        <v-col md="4" align="right">
+                            <v-avatar size="100">
+                            <img
+                                src="https://cdn.vuetifyjs.com/images/john.jpg"
+                                alt="John"
+                            >
+                            </v-avatar>
+                        </v-col>
+                        <v-col md="8" align="left">
+                            <ul>
+                                <h2 class="primary--text"> Etape 3: Créez vos produits et vendez </h2>
+                                <li>
+                                    Créez un maximum de produits pour augmenter vos ventes
+                                </li>
+                            </ul>
+                        </v-col>
+                    </v-row>
+                </v-col>
+            </v-container>
+            </v-tab-item>
+            <v-tab-item
+            >
+            <v-row>
+            <v-col align="left">
+            <v-row align="center">
+                <v-col md="3" align="right">
+                    <v-avatar size="100">
+                    <img
+                        src="https://cdn.vuetifyjs.com/images/john.jpg"
+                        alt="John"
+                    >
+                    </v-avatar>
+                </v-col>
+                <v-col md="8" align="left">
+                    <h3>
+                        Créez vos produits
+                    </h3>
+                    <p>
+                        Créer un bon contenu lorsque vous mettez vos articles en ligne est une étape importante pour obtenir plus de ventes. Des images de qualité et des descriptions détaillées incitent vos clients à cliquer sur vos produits et à les acheter.
+                    </p>
+                </v-col>
+            </v-row>
+            <v-row align="center">
+                <v-col md="3" align="right">
+                    <v-avatar size="100">
+                    <img
+                        src="https://cdn.vuetifyjs.com/images/john.jpg"
+                        alt="John"
+                    >
+                    </v-avatar>
+                </v-col>
+                <v-col md="8" align="left">
+                    <h3>
+                        Augmentez vos ventes
+                    </h3>
+                    <p>
+                        Il y a beaucoup de moyens d’augmenter vos ventes. En plus de proposer des produits à des prix compétitifs vous pouvez participer aux promotions Sos Shop et ainsi faire gagner de la visibilité à votre boutique. Optimiser les mots clés de vos articles et vous assurer que vous avez mis l’intégralité de vos produits dans la bonne catégorie sont d’autres moyens d’optimiser vos ventes .
+                    </p>    
+                </v-col>
+            </v-row>
+            <v-row align="center">
+                <v-col md="3" align="right">
+                    <v-avatar size="100">
+                    <img
+                        src="https://cdn.vuetifyjs.com/images/john.jpg"
+                        alt="John"
+                    >
+                    </v-avatar>
+                </v-col>
+                <v-col md="8" align="left">
+                    <h3>
+                        Gérez vos livraisons
+                    </h3>
+                    <p>
+                     Gérez vos livraisons dans les temps est un aspect essentiel de la satisfaction de vos clients finaux. Un client satisfait est un client qui revient.                    
+                     </p>
+                </v-col>
+            </v-row>
+            <v-row align="center">
+                <v-col md="3" align="right">
+                    <v-avatar size="100">
+                    <img
+                        src="https://cdn.vuetifyjs.com/images/john.jpg"
+                        alt="John"
+                    >
+                    </v-avatar>
+                </v-col>
+                <v-col md="8" align="left">
+                    <h3>
+                        Vos paiements
+                    </h3>
+                    <p>
+                        Il n’y a pas de frais pour mettre en ligne vos articles. Il ne vous est rien demandé de payer avant la vente de votre premier article.
+                    </p>
+                </v-col>
+            </v-row>  
+            </v-col> 
+            </v-row>
+            </v-tab-item>
+            </v-tabs>
+        </v-card>    
 
         </v-row>
         <v-row>
-
-        </v-row>
-        <v-row>
-            
+            <v-col align="center">
+                <v-btn color="primary">
+                    Commencer à vendre maintenant
+                </v-btn>
+            </v-col>          
         </v-row>
     </v-container>
 </template>
